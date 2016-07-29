2c0c40b1a563f54c3d93cb062fe14fd09b1d56fe
Julien <julien.coupe@orange.com>
2016-07-29 12:12:14 +0200

    Added MySQL service to our Application

--- src/main/java/com/orange/todolist/TodosServlet.java
+++ src/main/java/com/orange/todolist/TodosServlet.java
@@ -12,18 +12,18 @@ import javax.servlet.http.HttpServletResponse;
  * Unique servlet responsable de répondre aux GET/POST HTTP sur /todos
  */
 public class TodosServlet extends HttpServlet {
-	
+
 	public TodoStorage getStorageService(){
 		//In Memory storage
-		return new TodoStorageInMemory();
-		
+		//return new TodoStorageInMemory();
+
 		//Use Mysql on Kermit
-//		String login = System.getenv("MYSQL_USER");
-//		String password = System.getenv("MYSQL_PASSWORD");
-//		String host = System.getenv("WORKSHOP_DB_SERVICE_HOST");
-//		String port = System.getenv("WORKSHOP_DB_SERVICE_PORT");
-//		String database = System.getenv("MYSQL_DATABASE");
-//		return new TodoStorageJDBCMysql(host, port, database, login, password);
+		String login = System.getenv("MYSQL_USER");
+		String password = System.getenv("MYSQL_PASSWORD");
+		String host = System.getenv("WORKSHOP_DB_SERVICE_HOST");
+		String port = System.getenv("WORKSHOP_DB_SERVICE_PORT");
+		String database = System.getenv("MYSQL_DATABASE");
+		return new TodoStorageJDBCMysql(host, port, database, login, password);
 	}
 
 	@Override
@@ -32,7 +32,7 @@ public class TodosServlet extends HttpServlet {
 		String todosAsString = req.getParameter("todos");
 		getStorageService().put(todosAsString);
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -40,10 +40,10 @@ public class TodosServlet extends HttpServlet {
 		resp.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 		resp.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
 		resp.setHeader("Access-Control-Allow-Credentials", "true");
-		
+
 		PrintWriter writer = resp.getWriter();
 		writer.write(getStorageService().get().toString());
 		writer.close();
 	}
-	
+
 }
